0d206d4aef41a82f0571fdc319cd0ee9937bcc59
veriano <veriano@ukr.net>
2021-11-22 19:06:34 +0200

    processing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+const axios = require('axios');
+import templateFunction from './templates/gallery-card.hbs';
+import { fetchFotos }  from "./fetchFotos";
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', onSearchFormSubmit);
+
+function onSearchFormSubmit(e) {
+    const event = e.target.elements.searchQuery.value;
+     fetchFotos(event)
+   
+
+    
+};
+
