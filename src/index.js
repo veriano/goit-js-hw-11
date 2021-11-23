6cde64b5146059a099cb7d2671edbe267ec2e9db
veriano <veriano@ukr.net>
2021-11-23 19:06:01 +0200

    processing

--- src/index.js
+++ src/index.js
@@ -4,18 +4,51 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
 const axios = require('axios');
 import templateFunction from './templates/gallery-card.hbs';
-import { fetchFotos }  from "./fetchFotos";
 
 const form = document.getElementById('search-form');
+const input = form.elements.searchQuery;
 const gallery = document.querySelector('.gallery');
+let page = 1;
+let limitPerPage = 40;
+const totalEmages = 500;
+const totalPages = totalEmages/ limitPerPage;
 
 form.addEventListener('submit', onSearchFormSubmit);
 
-function onSearchFormSubmit(e) {
-    const event = e.target.elements.searchQuery.value;
-     fetchFotos(event)
-   
+function onSearchFormSubmit() {
+    const event = input.value;
 
-    
+    if (page > totalPages) {
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+
+    fetchFotos(event);
+    const markup = fotos.map(foto => templateFunction(foto));
+    gallery.innerHTML = markup;
+
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
 };
+
+async function fetchFotos(event) {
+    page += 1;
+    try {
+        const searchParams = new URLSearchParams({
+            image_type: 'photo',
+            orientation: "horizontal",
+            safesearch: true,
+            page: 1,
+            per_page: 40,
+        });
+        const BASE_URL = 'https://pixabay.com/api/';
+        const API_KEY = '24463326-9b2d5a427846ea9fa30299421';
+
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${event}&${searchParams}`);
+        const fotos = await response.blob();
+        return fotos;
+    } catch {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+};
+
 
